86c59fce8c64248b67f41cb2902e08984c812dad
Scecin <silvia.cecin@hotmail.com>
2022-04-14 13:10:31 +0100

    add todo event list

---
+++ controllers/controller.py
@@ -0,0 +1,4 @@
+from app import app
+from models.event import Event
+from models.todo_event import event_list
+
---
+++ models/todo_event.py
@@ -0,0 +1,7 @@
+from models.event import Event
+
+event_1 = Event("14 April", "CodeClan meetup", 10, "CodeClan Edinburgh", "General meeting")
+event_2 = Event("28 April", "Anna's birthday", 5, "Anna's house",
+"Anna's birthday party")
+
+event_list = [event_1, event_2]
