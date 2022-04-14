cbd0fb6f6b19e33416180c6f3e0ad68e7d5dfe61
Scecin <silvia.cecin@hotmail.com>
2022-04-14 15:34:51 +0100

    Advanced extensions almost completed

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,7 +2,7 @@ from asyncio import events
 from flask import render_template, request
 from app import app
 from models.event import Event
-from models.todo_event import add_new_event, event_list
+from models.todo_event import add_new_event, delete_event, event_list
 
 @app.route('/')
 def home(): 
@@ -15,6 +15,12 @@ def add_event():
     event_number_of_guests = request.form["number"]
     event_room_location = request.form["room"]
     event_description = request.form["description"]
-    new_event = Event(event_date, event_name_of_event, event_number_of_guests, event_room_location, event_description)
+    event_recurring = request.form["recurring"]
+    new_event = Event(event_date, event_name_of_event, event_number_of_guests, event_room_location, event_description, event_recurring)
     add_new_event(new_event)
     return render_template ("index.html", title="Home", events=event_list)
+
+@app.route('/deleteevent', methods=['POST'])
+def remove_event():
+    event_to_remove = event_list.index()
+    delete_event(event_to_remove)
--- models/todo_event.py
+++ models/todo_event.py
@@ -1,10 +1,13 @@
 from models.event import Event
 
-event_1 = Event("14 April", "CodeClan meetup", 10, "CodeClan Edinburgh", "General meeting")
+event_1 = Event("14 April", "CodeClan meetup", 10, "CodeClan Edinburgh", "General meeting", "True")
 event_2 = Event("28 April", "Anna's birthday", 5, "Anna's house",
-"Anna's birthday party")
+"Anna's birthday party", "False")
 
 event_list = [event_1, event_2]
 
 def add_new_event(new_event):
     event_list.append(new_event)
+
+def delete_event(old_event):
+    event_list.remove(old_event)
